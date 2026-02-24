3a677f2c8b0c81e80206136e35c6bff5a268a02e
unknown <faraxahmed011@gmail.com>
2026-02-24 15:18:05 +0500

    task app uploaded

---
+++ task-app/src/components/Card.jsx
@@ -0,0 +1,50 @@
+export function Card({dPath,hText,pText,buttons}) {
+  return (
+    <>
+      <div className="relative cursor-pointer border border-[#F5F5F5] group w-full h-full sm:h-99.25 min-w-75 transform transition-transform md:hover:scale-105 rounded-[10px] overflow-hidden">
+        <div className="absolute inset-0 bg-[#F8F8F8] rounded-[10px]"></div>
+        <div className="absolute -inset-16 opacity-0 md:group-hover:opacity-100 transition-opacity duration-300">
+          <img src="/card-comp-bg.webp"></img>
+        </div>
+        <div className="flex flex-col justify-around gap-4 w-full h-fit sm:h-99.25 min-w-75 relative z-10 p-6 md:group-hover:text-white transition-colors duration-300">
+          <span className="mb-10">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="46"
+              height="47"
+              viewBox="0 0 46 47"
+            >
+              <path
+                d={dPath}
+                fill="grey"
+              ></path>
+              <defs>
+                <linearGradient
+                  id="paint0_linear_910_3199"
+                  x1="22.8146"
+                  y1="-0.00390625"
+                  x2="22.8146"
+                  y2="46.9961"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop></stop>
+                  <stop offset="1" stop-opacity="0.3"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+          <span>
+            <h2 className="text-xl md:text-[22px] font-medium font-s1-pro-display ">{hText}</h2>
+            <p className="text-black md:group-hover:text-[#CFCFCF] transition-colors duration-300">{pText}</p>
+          </span>
+
+          <div className="flex flex-wrap gap-2">
+            {buttons.map((button,index)=>(
+              <a href="#" key={index}><button className="bg-[#FAFAFA] cursor-pointer text-black text-[15px] leading-6.25 font-s1-pro-display px-3 border border-[#CFCFCF] py-1 rounded-[5px] md:hover:bg-gray-200 transition-colors">{button.text}</button></a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- task-app/src/components/Header.jsx
+++ task-app/src/components/Header.jsx
@@ -1,22 +1,30 @@
-export function Header(){
-return(
-<>
-<nav className="flex justify-around fixed top-0 border-b border-[#e5e7eb]/80 z-50 translate-y-0 w-full">
-    <a href="#" className=" flex self-center w-50 min-w-42.5 h-10"><img src="/logo-dark.png" alt="logo.png" className="object-contain"></img></a>
-    <ul className="py-4 flex justify-between lg:flex gap-12.5 text-sm font-light font-inter text-nowrap items-center">
-        <li>Work</li>
-        <li>Career</li>
-        <li>Insights</li>
-    </ul>
+export function Header() {
+  return (
+    <>
 
-    <a href="#" className="self-center"><button className="px-5 text-nowrap py-1.5 cursor-pointer font-inter rounded-[60px] text-sm font-medium border border-black text-black bg-transparent hover:text-black/80 ">Contact</button></a>
-</nav>
+      <nav className="flex justify-around fixed top-0 border-b border-[#e5e7eb]/80 z-50 translate-y-0 w-full p-4 right-0 left-0 bg-white/95 text-black ">
 
+        <a href="#" className="flex self-center w-50 min-w-42.5 h-10">
+          <img
+            src="/logo-dark.png"
+            alt="logo.png"
+            className="object-contain"
+          ></img>
+        </a>
 
+        <ul className="py-4 flex justify-between lg:flex gap-12.5 text-sm font-light font-inter text-nowrap items-center">
+          <a href="#"><li>Work</li></a>
+          <a href="#"><li>Career</li></a>
+          <a href="#"><li>Insights</li></a>
+        </ul>
 
+        <a href="#" className="self-center">
+          <button className="px-5 text-nowrap py-1.5 cursor-pointer font-inter rounded-[60px] text-sm font-medium border border-black text-black bg-transparent hover:text-black/80 ">
+            Contact
+          </button>
+        </a>
+      </nav>
 
-
-</>
-
-)
+    </>
+  );
 }
